bb480da507b006524048658db1eaf9ab5e885c92
siam00000 <104170025+siam00000@users.noreply.github.com>
2022-04-23 19:21:53 +0600

    Update syestem.py

--- syestem.py
+++ syestem.py
@@ -88,51 +88,17 @@ def line_chack_dote():
 	sys.stdout.flush()
 	time.sleep(0.1)
     
-def main_apv():
-    ak="SIAM"
-    os.system('clear')
-    logo()
-    try:
-        key1=open('/data/data/com.termux/files/usr/bin/.siam-cov', 'r').read()
-    except IOError:
-        os.system("clear")
-        logo()
-        print ("[*]--------------------------------------------------------------")
-        print ("  Your Token Is Not Approved Already")
-        print ("[*]--------------------------------------------------------------")
-        print ("           THIS IS YOUR KEY BRO")
-        print ("[*]--------------------------------------------------------------")
-        print ("")
-        myid=uuid.uuid4().hex[:10].upper()
-        print ("          YOUR KEY : "+ak+myid)
-        print ("[*]--------------------------------------------------------------")
-        kok=open('/data/data/com.termux/files/usr/bin/.wasi-cov', 'w')
-        kok.close()
-        print ("")
-        print ("")
-        print ("     Copy Key And Sent Me Facebook  Approvel Your Key ")
-        print ("[*]--------------------------------------------------------------")
-        time.sleep(6)
-        os.system("xdg-open https://www.facebook.com/profile.php?id=100052683424097")
-        
-    r1=requests.get("https://pastebin.com/raw/sP7dL18J").text
-    if key1 in r1:
-        R()
-    else:
-        os.system("clear")
-        logo()
-        print ("[*]--------------------------------------------------------------")
-        print ("  Your Token Is Not Approved Already")
-        print ("[*]--------------------------------------------------------------")
-        print ("          THIS IS YOUR KEY BRO")
-        print ("[*]--------------------------------------------------------------")
-        print ("")
-        print ("          YOUR KEY : "+ak+key1)
-        print ("[*]--------------------------------------------------------------")
-        print ("     Copy Key And Sent Me FB Approvel Your Key ")
-        print ("[*]--------------------------------------------------------------")
-        time.sleep(3.5)
-        os.system("xdg-open https://www.facebook.com/profile.php?id=100052683424097")
+def mai
+
+    
+    
+  
+
+
+
+
+
+
 		
 def R():
 			os.system("clear")
